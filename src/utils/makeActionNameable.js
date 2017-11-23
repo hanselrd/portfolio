5cec3fd75c25b18a2b2113bcb8ca517028b51358
Hansel <18725263+hanselrd@users.noreply.github.com>
2017-11-23 04:11:44 -0500

    Nameable actions can now take place of normal actions

--- src/utils/makeActionNameable.js
+++ src/utils/makeActionNameable.js
@@ -1,9 +1,11 @@
 export default actionCreator => {
-  return (obj, ...args) => {
+  let nameableActionCreator = (obj, ...args) => {
     let action = actionCreator(...args);
     if (obj) {
       action.type = `${action.type} (${obj.__name})`;
     }
     return action;
   };
+  nameableActionCreator.toString = actionCreator.toString;
+  return nameableActionCreator;
 };
