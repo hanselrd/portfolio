000c337cd640587929bfffeda41d34c73283d09f
Hansel <18725263+hanselrd@users.noreply.github.com>
2017-11-26 18:39:18 -0500

    Finished implementing app bar

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,10 @@ import '../styles/Counter.css';
 import { RaisedButton } from 'material-ui';
 import PropTypes from 'prop-types';
 
+const style = {
+  margin: 7
+};
+
 class Counter extends Component {
   render() {
     const { counter, actions } = this.props;
@@ -11,14 +15,23 @@ class Counter extends Component {
         <p>Counter: {counter.count}</p>
         <div>
           <RaisedButton
-            label="increment"
+            label="Increment"
+            primary
+            style={style}
             onClick={() => actions.increment(counter)}
           />
           <RaisedButton
-            label="decrement"
+            label="Decrement"
+            primary
+            style={style}
             onClick={() => actions.decrement(counter)}
           />
-          <RaisedButton label="add 5" onClick={() => actions.add(counter, 5)} />
+          <RaisedButton
+            label="Add 5"
+            primary
+            style={style}
+            onClick={() => actions.add(counter, 5)}
+          />
         </div>
       </div>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { AppBar, FlatButton, IconMenu, MenuItem } from 'material-ui';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import PropTypes from 'prop-types';
+
+class Auth extends Component {
+  static muiName = 'FlatButton';
+
+  render() {
+    const { auth, actions } = this.props;
+    return (
+      <IconMenu
+        iconButtonElement={
+          <FlatButton
+            {...this.props}
+            labelPosition="before"
+            labelStyle={{ textTransform: 'none' }}
+            label={auth.user.displayName}
+            icon={<MoreVertIcon />}
+          />
+        }
+        targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+      >
+        <MenuItem primaryText="Help" />
+        <MenuItem
+          primaryText="Log out"
+          disabled={auth.logout.loading}
+          onClick={() => actions.logout()}
+        />
+      </IconMenu>
+    );
+  }
+}
+
+class Guest extends Component {
+  static muiName = 'FlatButton';
+
+  render() {
+    const { auth, actions } = this.props;
+    return (
+      <div>
+        <FlatButton
+          {...this.props}
+          label="Login"
+          disabled={auth.login.loading}
+          onClick={() => actions.login({ provider: 'google', type: 'popup' })}
+        />
+        <FlatButton {...this.props} label="Signup" />
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    const { auth, actions } = this.props;
+    return (
+      <AppBar
+        title={<small>Hansel De La Cruz</small>}
+        iconElementRight={
+          auth.user ? (
+            <Auth auth={auth} actions={actions} />
+          ) : (
+            <Guest auth={auth} actions={actions} />
+          )
+        }
+      />
+    );
+  }
+}
+
+Header.propTypes = {
+  auth: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+export default Header;
