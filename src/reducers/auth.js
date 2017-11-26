840749cdb650bd5c5bf34b86d8b7ddd222d0e263
Hansel <18725263+hanselrd@users.noreply.github.com>
2017-11-26 13:58:21 -0500

    Cleaned up auth actions/reducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,32 +1,19 @@
 import { createReducer } from 'redux-act';
-import { _start, _login, _logout, _error } from '../actions/auth';
+import { userFound, userNotFound } from '../actions/auth';
 
 const initialState = {
-  running: false,
-  user: null,
-  error: null
+  user: null
 };
 
 export default createReducer(
   {
-    [_start]: state => ({
+    [userFound]: (state, payload) => ({
       ...state,
-      running: true
-    }),
-    [_login]: (state, payload) => ({
-      ...state,
-      running: false,
       user: payload
     }),
-    [_logout]: state => ({
+    [userNotFound]: state => ({
       ...state,
-      running: false,
       user: null
-    }),
-    [_error]: (state, payload) => ({
-      ...state,
-      running: false,
-      error: payload
     })
   },
   initialState
