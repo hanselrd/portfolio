f6034af0a88e834f2c20f21ba4839624e49c301d
Hansel <hansel809@hotmail.com>
2017-11-26 13:58:21 -0500

    Cleaned up auth actions/reducer

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,76 +1,61 @@
 import { createAction } from 'redux-act';
+import { createActionAsync } from 'redux-act-async';
 import firebase from '../firebase';
 
-/*
-  _ signifies these should never be called implicitly
-  and are for internal use only
-*/
-export const _start = createAction('@@auth/start');
-export const _login = createAction('@@auth/login');
-export const _logout = createAction('@@auth/logout');
-export const _error = createAction('@@auth/error');
+export const start = createAction('@@auth/start');
+export const userFound = createAction('@@auth/user found');
+export const userNotFound = createAction('@@auth/user not found');
 
 export const handleAuthStateChanged = () => {
   return dispatch => {
-    dispatch(_start());
-    firebase.auth().onAuthStateChanged(
-      auth => {
-        if (auth) dispatch(_login(auth));
-        else dispatch(_logout());
-      },
-      authError => {
-        dispatch(_error(authError));
-      }
-    );
+    dispatch(start());
+    firebase.auth().onAuthStateChanged(auth => {
+      if (auth) dispatch(userFound(auth));
+      else dispatch(userNotFound());
+    });
   };
 };
 
-export const login = credentials => {
-  return dispatch => {
-    if (
-      credentials.hasOwnProperty('email') &&
-      credentials.hasOwnProperty('password')
-    ) {
-      const { email, password } = credentials;
-      firebase.auth().signInWithEmailAndPassword(email, password);
-    } else if (
-      credentials.hasOwnProperty('provider') &&
-      credentials.hasOwnProperty('type')
-    ) {
-      const { provider, type } = credentials;
-      let authProvider = null;
-      switch (provider) {
-        case 'google':
-          authProvider = new firebase.auth.GoogleAuthProvider();
-          break;
-        case 'facebook':
-          authProvider = new firebase.auth.FacebookAuthProvider();
-          break;
-        case 'twitter':
-          authProvider = new firebase.auth.TwitterAuthProvider();
-          break;
-        case 'github':
-          authProvider = new firebase.auth.GithubAuthProvider();
-          break;
-        default:
-          throw new Error('Invalid provider');
-      }
-      switch (type) {
-        case 'popup':
-          firebase.auth().signInWithPopup(authProvider);
-          break;
-        case 'redirect':
-          firebase.auth().signInWithRedirect(authProvider);
-          break;
-        default:
-          throw new Error('Invalid type');
-      }
+export const login = createActionAsync('@@auth/login', credentials => {
+  if (
+    credentials.hasOwnProperty('email') &&
+    credentials.hasOwnProperty('password')
+  ) {
+    const { email, password } = credentials;
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  } else if (
+    credentials.hasOwnProperty('provider') &&
+    credentials.hasOwnProperty('type')
+  ) {
+    const { provider, type } = credentials;
+    let authProvider = null;
+    switch (provider) {
+      case 'google':
+        authProvider = new firebase.auth.GoogleAuthProvider();
+        break;
+      case 'facebook':
+        authProvider = new firebase.auth.FacebookAuthProvider();
+        break;
+      case 'twitter':
+        authProvider = new firebase.auth.TwitterAuthProvider();
+        break;
+      case 'github':
+        authProvider = new firebase.auth.GithubAuthProvider();
+        break;
+      default:
+        throw new Error('Invalid provider');
     }
-  };
-};
+    switch (type) {
+      case 'popup':
+        return firebase.auth().signInWithPopup(authProvider);
+      case 'redirect':
+        return firebase.auth().signInWithRedirect(authProvider);
+      default:
+        throw new Error('Invalid type');
+    }
+  }
+});
 
-export const logout = () => {
-  return dispatch => {
-    firebase.auth().signOut();
-  };
-};
+export const logout = createActionAsync('@@auth/logout', () => {
+  return firebase.auth().signOut();
+});
