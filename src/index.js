f25a25f0dcc0fce3a0c69761431fc6d2279fa5c9
Hansel <hansel809@hotmail.com>
2017-11-27 23:57:36 -0500

    Switched from material-ui to antd

--- src/index.js
+++ src/index.js
@@ -3,22 +3,12 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import { Provider } from 'react-redux';
 import store from './store';
-import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
-import { grey700 } from 'material-ui/styles/colors';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: grey700
-  }
-});
-
 ReactDOM.render(
   <Provider store={store}>
-    <MuiThemeProvider muiTheme={muiTheme}>
-      <App />
-    </MuiThemeProvider>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
