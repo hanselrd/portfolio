e64a266d752286133e5f5a85d62e4ca3c7f70b06
Hansel <hansel809@hotmail.com>
2017-11-30 20:19:06 -0500

    Mobile menu is now fully functional

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,51 +12,82 @@ class Header extends Component {
         <Menu fixed="top" inverted>
           <Container>
             <Responsive as={Aux} maxWidth={Responsive.onlyTablet.maxWidth}>
-              <Menu.Item as="a">
-                <Icon name="sidebar" size="large" />
-              </Menu.Item>
+              <Dropdown item icon={<Icon name="sidebar" size="large" />}>
+                <Dropdown.Menu>
+                  <Dropdown.Header>Navigation</Dropdown.Header>
+                  <Dropdown.Item as={NavLink} to="/home">
+                    Home
+                  </Dropdown.Item>
+                  <Dropdown.Item as={NavLink} to="/blog">
+                    Blog
+                  </Dropdown.Item>
+                  <Dropdown.Item as={NavLink} to="/projects">
+                    Projects
+                  </Dropdown.Item>
+                  <Dropdown.Item as={NavLink} to="/cv">
+                    CV
+                  </Dropdown.Item>
+                  {!auth.user && (
+                    <Dropdown.Item as={NavLink} to="/login">
+                      Login
+                    </Dropdown.Item>
+                  )}
+                  {auth.user && (
+                    <Aux>
+                      <Dropdown.Header>{auth.user.displayName}</Dropdown.Header>
+                      <Dropdown.Item as={NavLink} to="/profile">
+                        Profile
+                      </Dropdown.Item>
+                      <Dropdown.Item as={NavLink} to="/settings">
+                        Settings
+                      </Dropdown.Item>
+                      <Dropdown.Item onClick={() => authActions.logout()}>
+                        Log out
+                      </Dropdown.Item>
+                    </Aux>
+                  )}
+                </Dropdown.Menu>
+              </Dropdown>
             </Responsive>
             <Menu.Item as={Link} to="/home" header>
               <Icon name="bookmark outline" size="big" color="blue" />
               Hansel De La Cruz
             </Menu.Item>
-            <Responsive
-              as={Menu.Menu}
-              position="right"
-              minWidth={Responsive.onlyComputer.minWidth}
-            >
-              <Menu.Item as={NavLink} to="/home">
-                Home
-              </Menu.Item>
-              <Menu.Item as={NavLink} to="/blog">
-                Blog
-              </Menu.Item>
-              <Menu.Item as={NavLink} to="/projects">
-                Projects
-              </Menu.Item>
-              <Menu.Item as={NavLink} to="/cv">
-                CV
-              </Menu.Item>
-              {!auth.user && (
-                <Menu.Item as={NavLink} to="/login">
-                  Login
+            <Responsive as={Aux} minWidth={Responsive.onlyComputer.minWidth}>
+              <Menu.Menu position="right">
+                <Menu.Item as={NavLink} to="/home">
+                  Home
                 </Menu.Item>
-              )}
-              {auth.user && (
-                <Dropdown item simple text={auth.user.displayName}>
-                  <Dropdown.Menu>
-                    <Dropdown.Item as={NavLink} to="/profile">
-                      Profile
-                    </Dropdown.Item>
-                    <Dropdown.Item as={NavLink} to="/settings">
-                      Settings
-                    </Dropdown.Item>
-                    <Dropdown.Item onClick={() => authActions.logout()}>
-                      Log out
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              )}
+                <Menu.Item as={NavLink} to="/blog">
+                  Blog
+                </Menu.Item>
+                <Menu.Item as={NavLink} to="/projects">
+                  Projects
+                </Menu.Item>
+                <Menu.Item as={NavLink} to="/cv">
+                  CV
+                </Menu.Item>
+                {!auth.user && (
+                  <Menu.Item as={NavLink} to="/login">
+                    Login
+                  </Menu.Item>
+                )}
+                {auth.user && (
+                  <Dropdown item simple text={auth.user.displayName}>
+                    <Dropdown.Menu>
+                      <Dropdown.Item as={NavLink} to="/profile">
+                        Profile
+                      </Dropdown.Item>
+                      <Dropdown.Item as={NavLink} to="/settings">
+                        Settings
+                      </Dropdown.Item>
+                      <Dropdown.Item onClick={() => authActions.logout()}>
+                        Log out
+                      </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                )}
+              </Menu.Menu>
             </Responsive>
           </Container>
         </Menu>
