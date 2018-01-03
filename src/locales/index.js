6cddbeb030bdd6083097ca2954d6ddf977d643d4
Ubuntu <18725263+hanselrd@users.noreply.github.com>
2018-01-03 22:13:31 +0000

    Cleaned up localization code

---
+++ src/locales/index.js
@@ -0,0 +1,12 @@
+import LocalizedStrings from 'react-localization';
+import en from './en';
+import es from './es';
+import ja from './ja';
+
+const strings = new LocalizedStrings({
+  en,
+  es,
+  ja
+});
+
+export default strings;
