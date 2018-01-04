8964751f31f433d0ad6ce8cfd3af460359df5cae
Ubuntu <18725263+hanselrd@users.noreply.github.com>
2018-01-04 03:56:59 +0000

    Fixed bug where ui would not update when locale changed

---
+++ src/actions/locale.js
@@ -0,0 +1,4 @@
+import { createAction } from 'redux-act';
+
+export const localeStart = createAction('@@locale/start');
+export const localeChange = createAction('@@locale/change');
--- src/utils/mapStateToProps.js
+++ src/utils/mapStateToProps.js
@@ -1,5 +1,6 @@
 export default state => ({
   auth: state.auth,
   counter1: state.counter1,
-  counter2: state.counter2
+  counter2: state.counter2,
+  locale: state.locale
 });
