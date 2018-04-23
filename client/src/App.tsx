53f8676fed3d20b14196f4836fe50852d88d6e0f
unknown <18725263+hanselrd@users.noreply.github.com>
2018-04-23 15:14:17 -0400

    Added firebase

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,20 +3,19 @@ import * as React from 'react';
 
 class App extends React.Component {
   public componentWillMount() {
+    console.log(process.env.REACT_APP_STAGE);
+
     firebase.auth().onAuthStateChanged(auth => {
       if (auth) {
         console.log(auth.toJSON());
-        auth.getIdToken().then(value => {
-          // localStorage.setItem(process.env.REACT_APP_AUTH_KEY as string, value);
-        });
       } else {
-        // localStorage.removeItem(process.env.REACT_APP_AUTH_KEY as string);
+        console.log(null);
       }
     });
   }
 
   public login = (e: React.MouseEvent<HTMLButtonElement>) => {
-    firebase.auth().signInWithPopup(new firebase.auth.FacebookAuthProvider());
+    firebase.auth().signInWithEmailAndPassword('test1@gmail.com', '123456');
   };
 
   public logout = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -27,7 +26,7 @@ class App extends React.Component {
     return (
       <div>
         <p>Hello</p>
-        <button onClick={this.login}>Sign in with Facebook</button>
+        <button onClick={this.login}>Sign in with Email</button>
         <button onClick={this.logout}>Sign out</button>
       </div>
     );
