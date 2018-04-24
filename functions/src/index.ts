3369e300d44b1a27dafee903a5959c92c1eecd43
unknown <18725263+hanselrd@users.noreply.github.com>
2018-04-23 23:05:26 -0400

    Added settings handler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,13 +3,43 @@ import * as functions from 'firebase-functions';
 
 admin.initializeApp(functions.config().firebase);
 
-export const userCreated = functions.auth.user().onCreate(user => {
-  return admin
+export const userCreated = functions.auth.user().onCreate(async user => {
+  await admin
     .database()
     .ref('/users')
     .child(user.uid)
     .set({
-      displayName: user.displayName,
+      email: user.email,
+      displayName: user.displayName ? user.displayName : 'Anonymous',
+      provider:
+        user.providerData.length > 0 ? user.providerData[0].providerId : null,
       createdAt: admin.database.ServerValue.TIMESTAMP
     });
+
+  const readwriteRef = admin
+    .database()
+    .ref('/readwrite')
+    .child(user.uid);
+
+  await readwriteRef.child('/settings').set({
+    showEmail: true
+  });
+
+  return;
 });
+
+export const settingsEditted = functions.database
+  .ref('/readwrite/{uid}/settings')
+  .onWrite(async (change, context) => {
+    const uid = context.params.uid;
+    const settings = change.after.val();
+    const user = await admin.auth().getUser(uid);
+
+    return admin
+      .database()
+      .ref('/users')
+      .child(uid)
+      .update({
+        email: settings.showEmail ? user.email : null
+      });
+  });
