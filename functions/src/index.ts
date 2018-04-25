1af2eb0b17b8a02897695f423211d477f2ba35db
unknown <18725263+hanselrd@users.noreply.github.com>
2018-04-25 18:18:27 -0400

    Added safer rules and implemented chat

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,41 +9,46 @@ export const userCreated = functions.auth.user().onCreate(async user => {
     .ref('/users')
     .child(user.uid)
     .set({
-      displayName: user.displayName ? user.displayName : 'Anonymous',
+      name: user.displayName ? user.displayName : 'Anonymous',
       photoURL: user.photoURL ? user.photoURL : null
     });
 
   await admin
     .database()
-    .ref('/protected')
+    .ref('/metadata/users')
     .child(user.uid)
     .set({
       email: user.email,
       provider:
         user.providerData.length > 0 ? user.providerData[0].providerId : null,
-      createdAt: admin.database.ServerValue.TIMESTAMP
+      created: admin.database.ServerValue.TIMESTAMP
+    });
+
+  await admin
+    .database()
+    .ref('/settings')
+    .child(user.uid)
+    .set({
+      showEmail: true
     });
 
   return;
 });
 
-export const newChatMessage = functions.database
-  .ref('/private/{uid}/writeable/chat/message')
+export const settingsEditted = functions.database
+  .ref('/settings/{userid}')
   .onWrite(async (change, context) => {
-    const uid = context.params.uid;
-    const data = change.after.val();
+    const userid = context.params.userid;
+    const settings = change.after.val();
+    const user = await admin.auth().getUser(userid);
 
-    if (data) {
-      return admin
-        .database()
-        .ref('/chat/messages')
-        .push()
-        .set({
-          user: uid,
-          text: data,
-          createdAt: admin.database.ServerValue.TIMESTAMP
-        });
-    }
+    await admin
+      .database()
+      .ref('/metadata/users')
+      .child(userid)
+      .update({
+        email: settings.showEmail ? user.email : null
+      });
 
     return;
   });
