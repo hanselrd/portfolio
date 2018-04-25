7a94d4a67bf2e5f261bcbd88d3c3529d8918f651
unknown <18725263+hanselrd@users.noreply.github.com>
2018-04-25 01:39:59 -0400

    Rewrote database rules

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,40 +9,26 @@ export const userCreated = functions.auth.user().onCreate(async user => {
     .ref('/users')
     .child(user.uid)
     .set({
-      email: user.email,
       displayName: user.displayName ? user.displayName : 'Anonymous',
+      photoURL: user.photoURL ? user.photoURL : null
+    });
+
+  await admin
+    .database()
+    .ref('/protected')
+    .child(user.uid)
+    .set({
+      email: user.email,
       provider:
         user.providerData.length > 0 ? user.providerData[0].providerId : null,
       createdAt: admin.database.ServerValue.TIMESTAMP
     });
 
-  const readwriteRef = admin
-    .database()
-    .ref('/readwrite')
-    .child(user.uid);
-
-  await readwriteRef.child('settings').set({
-    showEmail: true
-  });
-
   return;
 });
 
-export const userWritten = functions.database
-  .ref('/writeonly/{uid}/user')
-  .onWrite((change, context) => {
-    const uid = context.params.uid;
-    const data = change.after.val();
-
-    return admin
-      .database()
-      .ref('/users')
-      .child(uid)
-      .update(data);
-  });
-
-export const chatWritten = functions.database
-  .ref('/writeonly/{uid}/chat')
+export const newChatMessage = functions.database
+  .ref('/userspace/{uid}/writeable/chat/message')
   .onWrite(async (change, context) => {
     const uid = context.params.uid;
     const data = change.after.val();
@@ -50,30 +36,14 @@ export const chatWritten = functions.database
     if (data) {
       return admin
         .database()
-        .ref('/chat')
+        .ref('/chat/messages')
         .push()
         .set({
-          ...data,
-          author: uid,
+          user: uid,
+          text: data,
           createdAt: admin.database.ServerValue.TIMESTAMP
         });
     }
 
     return;
-  });
-
-export const settingsEditted = functions.database
-  .ref('/readwrite/{uid}/settings')
-  .onWrite(async (change, context) => {
-    const uid = context.params.uid;
-    const settings = change.after.val();
-    const user = await admin.auth().getUser(uid);
-
-    return admin
-      .database()
-      .ref('/users')
-      .child(uid)
-      .update({
-        email: settings.showEmail ? user.email : null
-      });
   });
