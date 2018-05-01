8260ecbd3d3c0507fc69b32b3502178eb33873d3
unknown <hansel809@hotmail.com>
2018-05-01 14:13:13 -0400

    Added router duck

--- client/src/ducks/index.ts
+++ client/src/ducks/index.ts
@@ -2,20 +2,28 @@ import { combineReducers } from 'redux';
 import { combineEpics } from 'redux-observable';
 import authReducer, { AuthEpic, AuthState, authEpic } from './auth';
 import chatReducer, { ChatEpic, ChatState, chatEpic } from './chat';
+import routerReducer, { RouterEpic, RouterState, routerEpic } from './router';
 import usersReducer, { UsersEpic, UsersState, usersEpic } from './users';
 
-export type RootEpic = AuthEpic | ChatEpic | UsersEpic;
+export type RootEpic = AuthEpic | ChatEpic | RouterEpic | UsersEpic;
 
-export const rootEpic = combineEpics<RootEpic>(authEpic, chatEpic, usersEpic);
+export const rootEpic = combineEpics<RootEpic>(
+  authEpic,
+  chatEpic,
+  routerEpic,
+  usersEpic
+);
 
 export type RootState = Readonly<{
   auth: AuthState;
   chat: ChatState;
+  router: RouterState;
   users: UsersState;
 }>;
 
 export default combineReducers<RootState>({
   auth: authReducer,
   chat: chatReducer,
+  router: routerReducer,
   users: usersReducer
 });
