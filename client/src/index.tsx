bdf7fe270a5d82e9b13a954fd79506d3a4ac5d15
unknown <hansel809@hotmail.com>
2018-04-30 23:18:24 -0400

    Added react-router along with history

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,18 @@
+import history from '@app/core/history';
 import store from '@app/core/redux';
 import * as React from 'react';
 // import registerServiceWorker from './registerServiceWorker';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router history={history}>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
