2c19e7119d7c68a0c1f7c0a3b2f69e833d40a2f5
unknown <hansel809@hotmail.com>
2018-04-30 22:23:42 -0400

    Updated packages for functions

--- client/src/core/redux.ts
+++ client/src/core/redux.ts
@@ -1,7 +1,7 @@
 import rootReducer, { RootState, rootEpic } from '@app/ducks';
 import { Store, applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import { createEpicMiddleware } from 'redux-observable';
 
 let store: Store<RootState>;
@@ -11,7 +11,7 @@ const epicMiddleware = createEpicMiddleware(rootEpic as any);
 if (process.env.NODE_ENV !== 'production') {
   store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(logger, epicMiddleware))
+    composeWithDevTools(applyMiddleware(/*logger,*/ epicMiddleware))
   );
 } else {
   store = createStore(rootReducer, applyMiddleware(epicMiddleware));
