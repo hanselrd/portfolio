fef08e416cec4cdd6ab938290006cfaf6ae9f20a
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2020-01-18 17:28:24 -0500

    add footer singleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
-import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { animated, useSpring } from 'react-spring';
 import { RootState } from './ducks';
 import { localeActions } from './ducks/locale';
 import { routerActions } from './ducks/router';
+import Footer from './singletons/Footer';
 import Header from './singletons/Header';
 import Routes from './singletons/Routes';
 
@@ -36,33 +36,7 @@ const App: React.FC = () => {
         <animated.span>{spring.number.interpolate(x => Math.floor(x))}</animated.span>
         <p>{JSON.stringify(window.location)}</p>
       </animated.main>
-      <footer className="px-4 py-16 mt-4 text-white bg-gray-900">
-        <div className="flex justify-between">
-          <div className="text-sm font-semibold">
-            <ul className="flex">
-              <li>About Me</li>
-              <li className="ml-2">Contact</li>
-            </ul>
-          </div>
-          <div className="text-lg">
-            <ul className="flex">
-              <li>
-                <FaFacebook />
-              </li>
-              <li className="ml-3">
-                <FaLinkedin />
-              </li>
-              <li className="ml-3">
-                <FaGithub />
-              </li>
-            </ul>
-          </div>
-        </div>
-        <hr className="my-8 border-0 border-gray-800" />
-        <div className="w-full text-xs text-center text-gray-300">
-          <span>&copy; Copyright 2020</span> <span className="font-bold">Hansel De La Cruz</span>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };
