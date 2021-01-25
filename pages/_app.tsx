cfcd29ad3e9f0dfb0ba2b618bdd05f3804fec4b8
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2021-01-25 12:52:22 -0500

    add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { useStoreActions } from "@/core/store";
 import withProviders from "@/hocs/providers";
 import { DefaultSeo } from "next-seo";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import React, { useEffect } from "react";
 
 const App: React.FC<AppProps> = (props) => {
@@ -15,6 +16,15 @@ const App: React.FC<AppProps> = (props) => {
 
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#333333" />
+        <meta name="msapplication-TileColor" content="#333333" />
+        <meta name="theme-color" content="#333333" />
+      </Head>
       <DefaultSeo
         title="Hansel De La Cruz"
         description="Hansel De La Cruz's Portfolio"
@@ -26,6 +36,20 @@ const App: React.FC<AppProps> = (props) => {
           description: "Hansel De La Cruz's Portfolio",
           locale: "en_US",
           site_name: "Hansel De La Cruz",
+          images: [
+            {
+              url: `${process.env.NEXT_PUBLIC_URL!}/logo-ogimage.png`,
+              width: 1200,
+              height: 630,
+              alt: "Logo",
+            },
+            {
+              url: `${process.env.NEXT_PUBLIC_URL!}/logo-transparent-ogimage.png`,
+              width: 1200,
+              height: 630,
+              alt: "Logo Transparent",
+            },
+          ],
         }}
       />
       <div className="flex flex-col min-h-screen bg-gray-100 font-poppins dark:bg-gray-900 dark:text-white">
