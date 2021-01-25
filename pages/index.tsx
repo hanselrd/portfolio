191f5be46d23c18328af050e4349b5da07d1fc6a
Hansel De La Cruz <hansel809@hotmail.com>
2021-01-25 11:01:54 -0500

    add SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
+import { NextSeo } from "next-seo";
 import Link from "next/link";
 import React from "react";
 
 const Index: React.FC = () => {
   return (
-    <div>
-      <Link href="/">
-        <a className="hover:underline">Home</a>
-      </Link>
-      <Link href="/projects">
-        <a className="hover:underline">Projects</a>
-      </Link>
-    </div>
+    <>
+      <NextSeo
+        title="Hansel De La Cruz | Home"
+        openGraph={{
+          title: "Hansel De La Cruz | Home",
+        }}
+      />
+      <div>
+        <Link href="/">
+          <a className="hover:underline">Home</a>
+        </Link>
+        <Link href="/projects">
+          <a className="hover:underline">Projects</a>
+        </Link>
+      </div>
+    </>
   );
 };
 
