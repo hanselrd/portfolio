5088b0e21a39cc42831233b59409b7400b5a03dc
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2021-01-25 23:04:26 -0500

    add projects data

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,24 +1,45 @@
+import projects, { Project } from "@/data/projects";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
 import React from "react";
 
-const ProjectsSlug: React.FC = () => {
+interface ProjectsSlugProps {
+  project: Project;
+}
+
+const ProjectsSlug: React.FC<ProjectsSlugProps> = (props) => {
   const router = useRouter();
   const slug = typeof router.query.slug === "string" ? router.query.slug : "";
 
   return (
     <>
       <NextSeo
-        title={`Hansel De La Cruz | Project ${slug}`}
+        title={`Hansel De La Cruz | ${props.project.title}`}
+        description={props.project.description}
         canonical={`${process.env.NEXT_PUBLIC_URL!}/projects/${slug}`}
         openGraph={{
           url: `${process.env.NEXT_PUBLIC_URL!}/projects/${slug}`,
-          title: `Hansel De La Cruz | Project ${slug}`,
+          title: `Hansel De La Cruz | ${props.project.title}`,
+          description: props.project.description,
         }}
       />
-      <div>Project {slug}</div>
+      <div>Project {JSON.stringify(props.project)}</div>
     </>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: projects.map((project) => ({ params: { slug: project.slug } })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<ProjectsSlugProps> = async (context) => {
+  return {
+    props: { project: projects.filter((project) => project.slug === context.params?.slug)[0] },
+  };
 };
 
 export default ProjectsSlug;
