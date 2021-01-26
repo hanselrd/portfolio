57ef3abe93b05858faa6cd5ddd248223453ea66c
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2021-01-26 13:25:37 -0500

    add DEV to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { DEV } from "@/core/environment";
 import React from "react";
 
 const Index: React.FC = () => {
   return (
     <>
       <div>Landing</div>
+      <div>{JSON.stringify({ DEV })}</div>
     </>
   );
 };
