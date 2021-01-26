6ca23f217d7350eae40b8b09cc8fcc366e3fd5e4
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2021-01-25 21:20:00 -0500

    remove list tags

--- containers/Footer.tsx
+++ containers/Footer.tsx
@@ -6,24 +6,14 @@ const Footer: React.FC = () => {
     <>
       <footer className="bg-black 2xl:px-40 px-6 py-20 text-white w-full sm:px-8 md:px-10 lg:px-14 xl:px-24">
         <div className="container space-y-10 flex flex-col mx-auto">
-          <div>
-            <ul className="space-x-8 flex justify-center font-medium text-sm">
-              <li>About Me</li>
-              <li>Contact</li>
-            </ul>
+          <div className="space-x-8 flex justify-center font-medium text-sm">
+            <span>About Me</span>
+            <span>Contact</span>
           </div>
-          <div>
-            <ul className="space-x-12 flex justify-center">
-              <li>
-                <FaFacebookSquare size={30} />
-              </li>
-              <li>
-                <FaGithub size={30} />
-              </li>
-              <li>
-                <FaLinkedinIn size={30} />
-              </li>
-            </ul>
+          <div className="space-x-12 flex justify-center">
+            <FaFacebookSquare size={30} />
+            <FaGithub size={30} />
+            <FaLinkedinIn size={30} />
           </div>
           <div className="space-x-1 flex justify-center text-xs text-gray-400">
             <span className="my-auto">
