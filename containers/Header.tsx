d760e1ca67214ae6305a1697c63ffaee25c358f8
Hansel De La Cruz <hansel809@hotmail.com>
2021-01-25 20:38:44 -0500

    add Footer

--- containers/Header.tsx
+++ containers/Header.tsx
@@ -1,6 +1,6 @@
 import { useStoreActions, useStoreState } from "@/core/store";
-import { BookmarkOutline, MenuAlt1, Moon, MoonOutline } from "heroicons-react";
 import React, { useState } from "react";
+import { HiMenuAlt1, HiMoon, HiOutlineBookmark, HiOutlineMoon } from "react-icons/hi";
 
 const Header: React.FC = () => {
   const themeState = useStoreState((state) => state.theme);
@@ -9,18 +9,18 @@ const Header: React.FC = () => {
 
   return (
     <>
-      <header className="bg-gray-100 dark:bg-gray-900 p-6 fixed left-0 top-0 shadow-md w-full z-10">
-        <div className="container flex justify-between mx-auto sm:px-2 md:px-4 lg:px-8 xl:px-16">
+      <header className="bg-gray-100 dark:bg-gray-900 p-6 2xl:px-40 fixed left-0 top-0 shadow-md w-full z-10 sm:px-8 md:px-10 lg:px-14 xl:px-24">
+        <div className="container flex justify-between mx-auto">
           <button
             onClick={() => {
               setShowMenu(!showMenu);
             }}
-            className="my-auto focus:outline-none transform hover:scale-125 transition-transform ease-in-out duration-500">
-            <MenuAlt1 size={30} />
+            className="my-auto focus:outline-none sm:transform sm:transition-transform sm:ease-in-out sm:duration-500 sm:hover:scale-125">
+            <HiMenuAlt1 size={30} />
           </button>
           <div className="flex my-auto">
             <span className="dark:text-gray-400 text-gray-600">
-              <BookmarkOutline size={30} />
+              <HiOutlineBookmark size={30} />
             </span>
             <span className="font-semibold text-lg my-auto">Hansel De La Cruz</span>
           </div>
@@ -28,15 +28,15 @@ const Header: React.FC = () => {
             onClick={() => {
               themeActions.toggleMode();
             }}
-            className="grid my-auto focus:outline-none transform hover:scale-125 transition-transform ease-in-out duration-500">
+            className="grid my-auto focus:outline-none sm:transform sm:transition-transform sm:ease-in-out sm:duration-500 sm:hover:scale-125">
             {themeState.mode &&
               (themeState.mode === "dark" ? (
                 <span className="col-start-1 row-start-1">
-                  <Moon size={30} />
+                  <HiMoon size={30} />
                 </span>
               ) : (
                 <span className="col-start-1 row-start-1">
-                  <MoonOutline size={30} />
+                  <HiOutlineMoon size={30} />
                 </span>
               ))}
           </button>
