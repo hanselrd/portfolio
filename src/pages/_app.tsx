a63bce3b2fc4246340a840f44f3bf1baf2fe0603
Hansel De La Cruz <hansel809@hotmail.com>
2021-01-27 01:00:25 -0500

    add projects data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -69,7 +69,7 @@ const App: React.FC<AppProps> = (props) => {
       />
       <div className="flex flex-col min-h-screen">
         <Header />
-        <main className="container flex-1 mt-20 p-6">
+        <main className="container flex-1 mt-20 p-6 break-all">
           <props.Component {...props.pageProps} />
         </main>
         <Footer />
