2b6dd395b25e301a7c60facc9321c3d7c3b0fb4a
Hansel De La Cruz <hansel809@hotmail.com>
2021-01-26 19:55:16 -0500

    add sitemap

--- src/core/environment.ts
+++ src/core/environment.ts
@@ -6,8 +6,4 @@ export const DEV =
   (BROWSER && window.location.hostname.startsWith("dev")) ||
   (process.env.VERCEL_GIT_COMMIT_REF ? process.env.VERCEL_GIT_COMMIT_REF === "dev" : false);
 
-export const NEXT_PUBLIC_URL = process.env.VERCEL_GIT_COMMIT_REF
-  ? process.env.VERCEL_GIT_COMMIT_REF === "dev"
-    ? process.env.NEXT_PUBLIC_DEV_URL!
-    : process.env.NEXT_PUBLIC_URL!
-  : "http://localhost:3000";
+export const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL!;
