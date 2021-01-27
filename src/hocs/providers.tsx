aa1fee53197758f8e3bd5d3425a35a99697a96b9
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2021-01-26 22:37:45 -0500

    add localization

--- src/hocs/providers.tsx
+++ src/hocs/providers.tsx
@@ -1,13 +1,17 @@
 import store from "@/core/store";
 import { StoreProvider } from "easy-peasy";
+import { I18nProps, I18nProvider } from "next-rosetta";
+import { AppProps } from "next/app";
 import React from "react";
 
-const withProviders = <P extends unknown>(Component: React.ComponentType<P>): React.FC<P> => {
+const withProviders = <P extends AppProps>(Component: React.ComponentType<P>): React.FC<P> => {
   const WithProvidersComponent: React.FC<P> = (props) => {
     return (
       <>
         <StoreProvider store={store}>
-          <Component {...props} />
+          <I18nProvider table={(props.pageProps as I18nProps).table}>
+            <Component {...props} />
+          </I18nProvider>
         </StoreProvider>
       </>
     );
---
+++ src/i18n/en.ts
@@ -0,0 +1,7 @@
+import { Table } from ".";
+
+export const table: Table = {
+  language: "English",
+  title: "Title",
+  welcome: "Welcome {{name}}!",
+};
