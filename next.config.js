b7c26c414c0a6821d81ed581293ec151a3a7a421
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2021-01-28 20:07:43 -0500

    disable locale detection

--- next.config.js
+++ next.config.js
@@ -1,15 +1,42 @@
 module.exports = {
   i18n: {
+    localeDetection: false,
     locales: [
       "en-US",
       // "ar-AR",
+      // "bg-BG",
+      // "bs-BA",
+      // "ca-ES",
+      // "da-DK",
+      // "de-DE",
+      // "el-GR",
+      // "en-CA",
+      // "en-GB",
       "es-LA"
       // "es-ES",
+      // "es-MX",
+      // "fa-IR",
+      // "fi-FI",
       // "fr-FR",
       // "fr-CA",
+      // "hi-IN",
+      // "hr-HR",
+      // "id-ID",
       // "it-IT",
+      // "ja-JP",
+      // "ko-KR",
+      // "mk-MK",
+      // "ms-MY",
+      // "pl-PL",
       // "pt-BR",
       // "pt-PT",
+      // "ro-RO",
+      // "ru-RU",
+      // "sl-SI",
+      // "sr-RS",
+      // "th-TH",
+      // "vi-VN",
+      // "zh-CN"
     ],
     defaultLocale: "en-US"
   }
