0a94af93c3d85f8feda0cba1f052a774e9dd57f3
Hansel De La Cruz <hansel809@hotmail.com>
2021-01-30 16:08:13 -0500

    make Footer responsive

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -12,20 +12,22 @@ const Footer: React.FC = () => {
     <>
       <footer className="bg-black px-6 py-20 text-white w-full sm:px-8 md:px-10 lg:px-14 xl:px-24 2xl:px-40">
         <div className="container space-y-10 flex flex-col mx-auto">
-          <div className="space-x-8 flex justify-center font-medium text-sm rtl:space-x-reverse">
-            <Link href="/about">{i18n.t("pages.about.title")}</Link>
-            <Link href="/contact">{i18n.t("pages.contact.title")}</Link>
-          </div>
-          <div className="space-x-12 flex justify-center rtl:space-x-reverse">
-            <Link external href="https://www.facebook.com/hansel.delacruz.14">
-              <FaFacebookSquare size={30} />
-            </Link>
-            <Link external href="https://github.com/hanselrd">
-              <FaGithub size={30} />
-            </Link>
-            <Link external href="https://www.linkedin.com/in/hansel-de-la-cruz-532199144">
-              <FaLinkedinIn size={30} />
-            </Link>
+          <div className="space-y-10 flex flex-col sm:space-y-0 sm:flex-row sm:justify-around">
+            <div className="space-x-8 flex justify-center font-medium text-sm rtl:space-x-reverse">
+              <Link href="/about">{i18n.t("pages.about.title")}</Link>
+              <Link href="/contact">{i18n.t("pages.contact.title")}</Link>
+            </div>
+            <div className="space-x-12 flex justify-center rtl:space-x-reverse">
+              <Link external href="https://www.facebook.com/hansel.delacruz.14">
+                <FaFacebookSquare size={30} />
+              </Link>
+              <Link external href="https://github.com/hanselrd">
+                <FaGithub size={30} />
+              </Link>
+              <Link external href="https://www.linkedin.com/in/hansel-de-la-cruz-532199144">
+                <FaLinkedinIn size={30} />
+              </Link>
+            </div>
           </div>
           <div dir="ltr" className="space-x-1 flex justify-center text-xs text-gray-400">
             <span className="my-auto">
